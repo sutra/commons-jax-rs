22a21eb44bb2225689869dada117391accde35ed
Sutra Zhou <zhoushuqun@gmail.com>
2018-05-11 11:57:18 +0800

    Remove deprecated classes.

--- commons-jax-rs-data/src/main/java/org/oxerr/commons/ws/rs/OffsetPageRequest.java
+++
@@ -1,8 +0,0 @@
-package org.oxerr.commons.ws.rs;
-
-@Deprecated
-public class OffsetPageRequest extends org.oxerr.commons.ws.rs.data.OffsetPageRequest {
-
-	private static final long serialVersionUID = 2017071101L;
-
-}
