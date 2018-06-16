1584802e178b9883b78fc48532f6f0d9b6c15e89
Sutra Zhou <zhoushuqun@gmail.com>
2018-06-16 20:17:47 +0800

    Add IllegalStateExceptionMapper.

---
+++ commons-jax-rs-exception-mapper/src/main/java/org/oxerr/commons/ws/rs/exceptionmapper/IllegalStateExceptionMapper.java
@@ -0,0 +1,26 @@
+package org.oxerr.commons.ws.rs.exceptionmapper;
+
+import javax.inject.Singleton;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * Maps {@link IllegalStateException} to {@link Response}.
+ */
+@Provider
+@Singleton
+public class IllegalStateExceptionMapper
+		implements ExceptionMapper<IllegalStateException> {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Response toResponse(IllegalStateException exception) {
+		return Response.status(Response.Status.BAD_REQUEST)
+			.entity(new ErrorEntity(null, exception.getMessage()))
+			.build();
+	}
+
+}
